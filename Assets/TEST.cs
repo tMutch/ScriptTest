e7253dd186d23b1b801b1c57015838d058215952
tMutch <takanao.t@r2.dion.ne.jp>
2018-10-06 13:29:43 +0900

    ScriptTest C#スクリプトの基礎
    変数及び関数の学習

---
+++ Assets/TEST.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+//using System.Collections.Generic;
+
+public class  Boss {
+	private int hp = 100;
+	private int power = 25;
+
+	public void Attack(){
+		Debug.Log (this.power + "のダメージを与えた");
+	}
+
+	public void Defence(int damage){
+		Debug.Log (damage + "のダメージを受けた");
+		this.hp -= damage;
+	}
+}
+
+public class TEST : MonoBehaviour {
+	// Use this for initialization
+	void Start(){
+		Boss midboss = new Boss ();
+		midboss.Attack ();
+		midboss.Defence (7);
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
